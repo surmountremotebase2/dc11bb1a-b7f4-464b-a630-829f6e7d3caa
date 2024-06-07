508471766a0d38f6670f635af859e607f88a4a2c
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-07 01:34:02 -0400

    User Joe Solomon saved changes to main.py at 06/07/2024, 05:34:02 UTC time.

--- 786a9627-c6af-4c18-ba52-04dd2815474c/main.py
+++ 786a9627-c6af-4c18-ba52-04dd2815474c/main.py
@@ -1,1 +1,19 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.data import OHLCV
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        # List of stock tickers to consider for the strategy
+        self.tickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "FB"]
+        # Preparing data requirement: OHLCV data for each ticker
+        self.data_list = [OHLCV(ticker) for ticker in self.tickers]
+
+    @property
+    def interval(self):
+        # Setting data interval to daily for analyzing the previous day's open-close performance
+        return "1day"
+
+    @property
+    def assets(self):
+        # Strategy will consider assets specified in
