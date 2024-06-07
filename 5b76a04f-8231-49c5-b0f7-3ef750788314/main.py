f66369d69035cd33c065a68769e67afe3bfa6f42
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-07 01:10:25 -0400

    User Joe Solomon saved changes to main.py at 06/07/2024, 05:10:25 UTC time.

--- 5b76a04f-8231-49c5-b0f7-3ef750788314/main.py
+++ 5b76a04f-8231-49c5-b0f7-3ef750788314/main.py
@@ -6,28 +6,28 @@ from surmount.data import Asset, InstitutionalOwnership, InsiderTrading
 class TradingStrategy(Strategy):
 
     def __init__(self):
-    self.tickers = ["SPY", "QQQ", "AAPL", "GOOGL"]
-        self.data_list = [InstitutionalOwnership(i) for i in self.tickers]
-        self.data_list += [InsiderTrading(i) for i in self.tickers]
+        self.tickers = ["SPY", "QQQ", "AAPL", "GOOGL"]
+            self.data_list = [InstitutionalOwnership(i) for i in self.tickers]
+            self.data_list += [InsiderTrading(i) for i in self.tickers]
 
-    @property
-    def interval(self):
-        return "1day"
+        @property
+        def interval(self):
+            return "1day"
 
-    @property
-    def assets(self):
-        return self.tickers
+        @property
+        def assets(self):
+            return self.tickers
 
-    @property
-    def data(self):
-        return self.data_list
+        @property
+        def data(self):
+            return self.data_list
 
-    def run(self, data):
-        allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
-        for i in self.data_list:
-            if tuple(i)[0]=="insider_trading":
-                if data[tuple(i)] and len(data[tuple(i)])>0:
-                    if "Sale" in data[tuple(i)][-1]['transactionType']:
-                        allocation_dict[tuple(i)[1]] = 0
+        def run(self, data):
+            allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
+            for i in self.data_list:
+                if tuple(i)[0]=="insider_trading":
+                    if data[tuple(i)] and len(data[tuple(i)])>0:
+                        if "Sale" in data[tuple(i)][-1]['transactionType']:
+                            allocation_dict[tuple(i)[1]] = 0
 
-        return TargetAllocation(allocation_dict)
+            return TargetAllocation(allocation_dict)
